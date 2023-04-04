b19c0b82b6826238f017d3eeae1d33b0bea17dbb
ketanDeshmukh <deshmukhketan647@gmail.com>
2023-04-05 04:02:47 +0530

    "Yes"

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -13,30 +13,19 @@ bookRoute.route('/').get(async(req,resp)=>{
   resp.send(result);
 })
 // Get Book
-bookRoute.route('/read-book/:id').get((req, res) => {
-    Book.findById(req.params.id, (error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      res.json(data)
-    }
-  })
-})
+bookRoute.route('/read-book/:id').get(async(req,resp)=>{
+  let result=await Book.findById({"_id":req.params.id});
+  resp.send(result);
+});
 
 // Update Book
-bookRoute.route('/update-book/:id').put((req, res, next) => {
-    Book.findByIdAndUpdate(req.params.id, {
-    $set: req.body
-  }, (error, data) => {
-    if (error) {
-      return next(error);
-      console.log(error)
-    } else {
-      res.json(data)
-      console.log('Book updated successfully!')
-    }
-  })
-})
+bookRoute.route('/update-book/:id').put(async(req,resp)=>{
+  let result=await Book.findByIdAndUpdate({"_id":req.params.id},
+{  $set: req.body}
+  );
+  resp.send(result);
+});
+
 // Delete Book
 bookRoute.route('/delete-book/:id').delete(async(req,resp)=>{
   let result=await Book.deleteOne({ "_id" : (req.params.id) });
